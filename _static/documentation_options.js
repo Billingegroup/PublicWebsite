1bd8c44adad7267789313b41a92f3eb374941bfe
sbillinge <sbillinge@users.noreply.github.com>
2023-01-24 21:48:50 +0000

    deploy: 7a4ddcf6fd3ff9fa48afddf7e77541abb29302e7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g6f00e07',
+    VERSION: '0.post1+g7a4ddcf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
